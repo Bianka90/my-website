e3000d7c1db8d0dfb30d986262b3f16e9cfa3507
William Romero Ramirez <william@williamromero.dev>
2025-12-12 19:08:36 +0100

    slide update

--- src/About.jsx
+++ src/About.jsx
@@ -1,26 +1,30 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useInView } from "./useInView";
 import bianka1 from "./assets/bianka_violin.jpeg";
 import bianka2 from "./assets/bianka_profile_bw.jpeg";
 import blackBg from "./assets/black_bg.jpg";
-import blackBgLs from "./assets/black_bg_ls.jpg";
+
 export default function About() {
-  const img1Ref = useRef();
-  const img2Ref = useRef();
+  const photosRef = useRef(null);
 
-  const isMobile = window.matchMedia("(max-width: 768px)").matches;
-  const margin = isMobile ? "0px 0px -50px 0px" : "0px 0px -200px 0px";
+  const [margin, setMargin] = useState("0px 0px -200px 0px");
+  useEffect(() => {
+    const mq = window.matchMedia("(max-width: 768px)");
+    const update = () =>
+      setMargin(mq.matches ? "0px 0px -50px 0px" : "0px 0px -200px 0px");
+    update();
+    mq.addEventListener?.("change", update);
+    return () => mq.removeEventListener?.("change", update);
+  }, []);
 
-  const img1InView = useInView(img1Ref, margin);
-  const img2InView = useInView(img2Ref, margin);
+  const inView = useInView(photosRef, margin);
 
   return (
     <section
       id="about"
-      className="max-w-screen h-screen bg-stone-900 flex flex-col pt-10 px-5 md:px-40 "
+      className="max-w-screen min-h-screen bg-stone-900 flex flex-col pt-10 px-5 md:px-40"
     >
-      {/* About Me */}
-      <div className=" md:w-full lg:mb-20">
+      <div className="md:w-full lg:mb-20">
         <div className="flex items-center">
           <h2 className="text-4xl text-white">About me</h2>
           <div className="border-b-4 border-white w-20 ml-5"></div>
@@ -32,38 +36,39 @@ export default function About() {
       </div>
 
       <div
+        ref={photosRef}
         id="photos"
-        className="relative w-full h-screen flex justify-center items-center mt-20"
+        className="relative w-full min-h-screen flex justify-center items-center mt-20"
+        style={{ perspective: "1000px" }}
       >
-        {/* Background image */}
         <img
           src={blackBg}
           alt="Background"
-          className="absolute top-[50px] left-[50%] w-[400px] h-[400px] transform -translate-x-1/2 z-0 "
+          className="absolute top-[50px] left-1/2 w-[400px] h-[400px] -translate-x-1/2 z-0"
           loading="lazy"
         />
 
-        {/* Middle image */}
-        <img
-          ref={img1Ref}
-          src={bianka1}
-          alt="Middle"
-          className={`absolute -top-[50px] left-[70%] w-[230px] md:left-[59%] md:w-[22%] md:-top-[100px] transform -translate-x-1/2 z-10 opacity-0 ${
-            img1InView ? "slide-in-right" : ""
+        {/* Right image */}
+        <div
+          className={`absolute -top-[50px] left-[70%] w-[230px] md:left-[59%] md:w-[22%] md:-top-[100px] -translate-x-1/2 z-10 ${
+            inView ? "slide-in-right" : "hidden-before"
           }`}
-          loading="lazy"
-        />
+        >
+          <img src={bianka1} alt="Bianka playing violin" loading="lazy" />
+        </div>
 
-        {/* Top image */}
-        <img
-          ref={img2Ref}
-          src={bianka2}
-          alt="Top"
-          className={`absolute top-[150px] left-[30%] w-[230px] md:left-[38%] md:w-[22%] md:top-[100px] xl: transform -translate-x-1/2 z-20 opacity-0 ${
-            img2InView ? "slide-in-left" : ""
+        {/* Left image */}
+        <div
+          className={`absolute top-[150px] left-[30%] w-[230px] md:left-[38%] md:w-[22%] md:top-[100px] -translate-x-1/2 z-20 ${
+            inView ? "slide-in-left" : "hidden-before"
           }`}
-          loading="lazy"
-        />
+        >
+          <img
+            src={bianka2}
+            alt="Bianka portrait black and white"
+            loading="lazy"
+          />
+        </div>
       </div>
     </section>
   );
